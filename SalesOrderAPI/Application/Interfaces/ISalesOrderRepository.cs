e29a6580c3ed3b74e2c68dc9e2d1dd833f99e8ab
Thushana LS <dhushanals@example.com>
2025-11-15 17:34:41 +0530

    feat(repository): add ISalesOrderRepository interface

---
+++ SalesOrderAPI/Application/Interfaces/ISalesOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using SalesOrderAPI.Domain.Entities;
+
+namespace SalesOrderAPI.Application.Interfaces
+{
+    public interface ISalesOrderRepository
+    {
+        Task<IEnumerable<SalesOrder>> GetAllAsync();
+        Task<SalesOrder?> GetByIdAsync(int id);
+        Task<SalesOrder> CreateAsync(SalesOrder salesOrder);
+        Task<SalesOrder> UpdateAsync(SalesOrder salesOrder);
+        Task<bool> DeleteAsync(int id);
+        Task<string> GenerateOrderNumberAsync();
+    }
+}
