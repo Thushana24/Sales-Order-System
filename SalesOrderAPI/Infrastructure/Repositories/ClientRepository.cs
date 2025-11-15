37a571a8bfca9059bf650dc71a1e5de0b15f2a66
Thushana LS <dhushanals@example.com>
2025-11-15 17:37:17 +0530

    feat(repository): implement ClientRepository

---
+++ SalesOrderAPI/Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesOrderAPI.Application.Interfaces;
+using SalesOrderAPI.Domain.Entities;
+using SalesOrderAPI.Infrastructure.Data;
+
+namespace SalesOrderAPI.Infrastructure.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ClientRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Client>> GetAllAsync()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        public async Task<Client?> GetByIdAsync(int id)
+        {
+            return await _context.Clients.FindAsync(id);
+        }
+    }
+}
