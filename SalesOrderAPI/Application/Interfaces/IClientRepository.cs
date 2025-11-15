49f32cb9c0d13cfe56d06ee17c0372d3a424344d
Thushana LS <dhushanals@example.com>
2025-11-15 17:31:14 +0530

    feat(repository): add IClientRepository interface

---
+++ SalesOrderAPI/Application/Interfaces/IClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using SalesOrderAPI.Domain.Entities;
+
+namespace SalesOrderAPI.Application.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<IEnumerable<Client>> GetAllAsync();
+        Task<Client?> GetByIdAsync(int id);
+    }
+}
