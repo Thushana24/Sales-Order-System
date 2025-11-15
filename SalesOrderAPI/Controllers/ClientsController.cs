6bcbf45302096573f18a69b730f0e61c60e89161
Thushana LS <dhushanals@example.com>
2025-11-15 17:53:05 +0530

    feat(controller): add ClientsController with GET endpoints

---
+++ SalesOrderAPI/Controllers/ClientsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesOrderAPI.Application.Interfaces;
+
+namespace SalesOrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IClientService _clientService;
+
+        public ClientsController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var clients = await _clientService.GetAllClientsAsync();
+            return Ok(clients);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var client = await _clientService.GetClientByIdAsync(id);
+            if (client == null)
+                return NotFound();
+            return Ok(client);
+        }
+    }
+}
