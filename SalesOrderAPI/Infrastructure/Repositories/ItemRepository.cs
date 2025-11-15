8c4f8729387d2fa10a6c8a04fefc6d2faeb673d1
Thushana LS <dhushanals@example.com>
2025-11-15 17:38:51 +0530

    feat(repository): implement ItemRepository

---
+++ SalesOrderAPI/Infrastructure/Repositories/ItemRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesOrderAPI.Application.Interfaces;
+using SalesOrderAPI.Domain.Entities;
+using SalesOrderAPI.Infrastructure.Data;
+
+namespace SalesOrderAPI.Infrastructure.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ItemRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Item>> GetAllAsync()
+        {
+            return await _context.Items.ToListAsync();
+        }
+
+        public async Task<Item?> GetByIdAsync(int id)
+        {
+            return await _context.Items.FindAsync(id);
+        }
+    }
+}
