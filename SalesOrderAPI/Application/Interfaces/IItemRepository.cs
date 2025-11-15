2be1fd5928b15b0a3a05f0ff053d8c49069f7c10
Thushana LS <dhushanals@example.com>
2025-11-15 17:33:25 +0530

    feat(repository): add IItemRepository interface

---
+++ SalesOrderAPI/Application/Interfaces/IItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using SalesOrderAPI.Domain.Entities;
+
+namespace SalesOrderAPI.Application.Interfaces
+{
+    public interface IItemRepository
+    {
+        Task<IEnumerable<Item>> GetAllAsync();
+        Task<Item?> GetByIdAsync(int id);
+    }
+}
